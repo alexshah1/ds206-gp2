2ab23e24c4de5d86d03caa953b0ffb5113c511c6
alexshah1 <alexandershahramanian@gmail.com>
2024-05-16 22:09:45 +0400

    uncommented fact

--- pipeline_dimensional_data/config.py
+++ pipeline_dimensional_data/config.py
@@ -2,4 +2,4 @@
 SQL_SERVER_CONFIG_FILE = "sql_server_config.cfg"
 
 # Table names - Dimensional
-TABLE_NAMES = ["dim_Region", "dim_Territories", "dim_Shippers", "dim_Categories", "dim_Customers", "dim_Suppliers", "dim_Employees", "dim_Products"]#, "fact_Orders"]
+TABLE_NAMES = ["dim_Region", "dim_Territories", "dim_Shippers", "dim_Categories", "dim_Customers", "dim_Suppliers", "dim_Employees", "dim_Products", "fact_Orders"]
